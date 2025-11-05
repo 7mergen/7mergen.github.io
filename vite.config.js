8fba331d74f69c85c493e9214575282073fc0e34
a11saints <a.magzat@kmg-s.kz>
2025-11-05 12:06:03 +0500

    Fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vue-olga-positivus/',
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
